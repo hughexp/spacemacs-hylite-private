155ddb9aad13a76957b95cdb34a1ca3ed5538c60
hugh <huyichao@follower-inc.com>
2018-06-10 23:56:30 +0800

    1.add ssh el;2.config for emax and msys64.

---
+++ layers/hylite-init/config.el
@@ -0,0 +1,34 @@
+
+;;;
+;;;init config for emax path.
+;;;
+
+(defvar emax-root (concat (expand-file-name "~") "/emax"))
+(defvar emax-bin (concat emax-root "/bin"))
+(defvar emax-bin64 (concat emax-root "/bin64"))
+(defvar emax-mingw64 (concat emax-root "/mingw64/bin"))
+(defvar emax-lisp (concat emax-root "/lisp"))
+
+(setq exec-path (cons emax-bin exec-path))
+(setenv "PATH" (concat emax-bin ";" (getenv "PATH")))
+
+(setq exec-path (cons emax-bin64 exec-path))
+(setenv "PATH" (concat emax-bin64 ";" (getenv "PATH")))
+
+(setq exec-path (cons emax-mingw64 exec-path))
+(setenv "PATH" (concat emax-mingw64 ";" (getenv "PATH")))
+
+(setenv "PATH" (concat "C:\\msys64\\usr\\bin;C:\\msys64\\mingw64\\bin;" (getenv "PATH")))
+
+;;(dolist (dir '("~/emax/" "~/emax/bin/" "~/emax/bin64/" "~/emax/mingw64/bin/" "~/emax/lisp/" "~/emax/elpa/"))
+;;(add-to-list 'load-path dir))
+
+;;(set-language-environment 'utf-8)
+;;(setq locale-coding-system 'utf-8)
+;;(set-default-coding-systems 'utf-8)
+;;(set-terminal-coding-system 'utf-8)
+;;(prefer-coding-system 'utf-8)
+
+;;;; Tangle configuration
+;;(org-babel-load-file (expand-file-name "~/emax/emax.org" user-emacs-directory))
+;;;;(garbage-collect))
