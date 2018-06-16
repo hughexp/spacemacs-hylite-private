d2ae3a2422d6caa9bf53c8e6ef816824725dd1cf
hugh <huyichao@follower-inc.com>
2018-06-16 23:11:52 +0800

    add org-mode config to hylite-note layer.

--- layers/hylite-note/config.el
+++ layers/hylite-note/config.el
@@ -1,1 +1,12 @@
 (setq leanote-api-root "http://45.77.112.239:9000/api")
+
+
+(defun hylite/org-ispell ()
+  "Configure `ispell-skip-region-alist' for `org-mode'."
+  (make-local-variable 'ispell-skip-region-alist)
+  (add-to-list 'ispell-skip-region-alist '(org-property-drawer-re))
+  (add-to-list 'ispell-skip-region-alist '("~" "~"))
+  (add-to-list 'ispell-skip-region-alist '("=" "="))
+  (add-to-list 'ispell-skip-region-alist '("^#\\+BEGIN_SRC" . "^#\\+END_SRC")))
+
+(add-hook 'org-mode-hook #'hylite/org-ispell)
